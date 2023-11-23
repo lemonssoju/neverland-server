538ce1decc309556b8dd8bde07c37db2a80dc33d
joonghyun <joongh@ewhain.net>
2023-11-23 21:29:45 +0900

    Initialize the project

---
+++ src/main/java/com/lesso/neverland/NeverlandApplication.java
@@ -0,0 +1,13 @@
+package com.lesso.neverland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverlandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeverlandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lesso/neverland/NeverlandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lesso.neverland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeverlandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
