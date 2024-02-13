b33e57e8118ebcf2bad968a25ea7e05eb9c81dac
joonghyun <joongh@ewhain.net>
2024-02-13 23:57:04 +0900

    #4 feat: User entity 생성

---
+++ src/main/java/com/lesso/neverland/user/domain/entity/User.java
@@ -0,0 +1,25 @@
+package com.lesso.neverland.user.domain.entity;
+
+import com.lesso.neverland.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userIdx;
+
+    @Column(nullable = false)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+}
