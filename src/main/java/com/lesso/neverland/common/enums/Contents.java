2f6f29f287118066805a28f27774e3f8f2a3526c
joonghyun <joongh@ewhain.net>
2024-02-14 22:40:22 +0900

    #4 feat: 콘텐츠와 취향을 enum 클래스로 생성

--- src/main/java/com/lesso/neverland/common/enums/Contents.java
+++ src/main/java/com/lesso/neverland/common/enums/Contents.java
@@ -2,7 +2,32 @@ package com.lesso.neverland.common.enums;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 @Getter
 public enum Contents {
-    MOVIE, DRAMA, ANIMATION, FASHION, MUSIC, ENTERTAINMENT
+
+    MOVIE("영화", null),
+        HORROR("호러", MOVIE),
+        ROMANCE("로맨스", MOVIE),
+    DRAMA("드라마", null),
+    ANIMATION("애니메이션", null),
+    FASHION("패션", null),
+    MUSIC("음악", null),
+    ENTERTAINMENT("예능", null);
+
+    private final String name;
+    private final Contents parent;
+    private final List<Contents> child;
+
+    Contents(String name, Contents parent) {
+        this.child = new ArrayList<>();
+        this.name = name;
+        this.parent = parent;
+        if(Objects.nonNull(parent)) {
+            parent.child.add(this);
+        }
+    }
 }
