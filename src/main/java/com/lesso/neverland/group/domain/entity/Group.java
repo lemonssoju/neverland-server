3637ace55c6a0826d3e0ddb16a208a3b969689c1
joonghyun <joongh@ewhain.net>
2024-02-14 20:47:02 +0900

    #4 feat: Group entity 생성

---
+++ src/main/java/com/lesso/neverland/group/domain/entity/Group.java
@@ -0,0 +1,29 @@
+package com.lesso.neverland.group.domain.entity;
+
+import com.lesso.neverland.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+
+@Entity
+@Getter
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Group extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long groupIdx;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false)
+    private String subName;
+
+    @Column(nullable = false)
+    private String groupImage;
+
+    // member
+}
