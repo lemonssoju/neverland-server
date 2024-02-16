3a5901ade0b01ceb130d5c9b7b9c7a78a06c385d
joonghyun <joongh@ewhain.net>
2024-02-16 20:51:55 +0900

    #4 fix: data source exclude 설정 삭제

--- src/main/java/com/lesso/neverland/user/repository/UserRepository.java
+++ src/main/java/com/lesso/neverland/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.lesso.neverland.user.repository;
 
 import com.lesso.neverland.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
