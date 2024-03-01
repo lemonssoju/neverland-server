656c5d4d485bd02feac2fbc51f8502d3b832a370
joonghyun <joongh@ewhain.net>
2024-03-02 05:11:18 +0900

    #20 feat: 그룹 피드 목록 조회 API 구현

--- src/main/java/com/lesso/neverland/group/application/GroupService.java
+++ src/main/java/com/lesso/neverland/group/application/GroupService.java
@@ -4,7 +4,11 @@ import com.lesso.neverland.common.BaseException;
 import com.lesso.neverland.group.domain.Team;
 import com.lesso.neverland.group.dto.GroupListDto;
 import com.lesso.neverland.group.dto.GroupListResponse;
+import com.lesso.neverland.group.dto.GroupPostDto;
+import com.lesso.neverland.group.dto.GroupPostListResponse;
 import com.lesso.neverland.group.repository.GroupRepository;
+import com.lesso.neverland.post.domain.Post;
+import com.lesso.neverland.post.repository.PostRepository;
 import com.lesso.neverland.user.application.UserService;
 import com.lesso.neverland.user.domain.User;
 import com.lesso.neverland.user.repository.UserRepository;
@@ -14,8 +18,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static com.lesso.neverland.common.BaseResponseStatus.DATABASE_ERROR;
-import static com.lesso.neverland.common.BaseResponseStatus.INVALID_USER_IDX;
+import static com.lesso.neverland.common.BaseResponseStatus.*;
+import static com.lesso.neverland.common.constants.Constants.ACTIVE;
 
 @Service
 @RequiredArgsConstructor
@@ -24,12 +28,13 @@ public class GroupService {
     private final UserService userService;
     private final UserRepository userRepository;
     private final GroupRepository groupRepository;
+    private final PostRepository postRepository;
 
     // 그룹 목록 조회
     public GroupListResponse getGroupList() throws BaseException {
         try {
             User user = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
-            List<Team> groupList = groupRepository.findByAdmin(user);
+            List<Team> groupList = groupRepository.findByAdminAndStatusEquals(user, ACTIVE);
             List<GroupListDto> groupListDto = groupList.stream()
                     .map(group -> new GroupListDto(
                             group.getTeamIdx(),
@@ -37,6 +42,27 @@ public class GroupService {
                             group.getName(),
                             group.getSubName())).collect(Collectors.toList());
             return new GroupListResponse(groupListDto);
+        } catch (BaseException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new BaseException(DATABASE_ERROR);
+        }
+    }
+
+    // 그룹 피드 목록 조회
+    public GroupPostListResponse getGroupPostList(Long groupIdx) throws BaseException {
+        try {
+            User user = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+            Team group = groupRepository.findById(groupIdx).orElseThrow(() -> new BaseException(INVALID_GROUP_IDX));
+            List<Post> groupPostList = postRepository.findByTeamAndStatusEqualsOrderByCreatedDateDesc(group, ACTIVE);
+
+            List<GroupPostDto> groupPostListDto = groupPostList.stream()
+                    .map(groupPost -> new GroupPostDto(
+                            groupPost.getTitle(),
+                            groupPost.getSubtitle(),
+                            groupPost.getPostImage(),
+                            groupPost.getUser().getProfile().getNickname())).collect(Collectors.toList());
+            return new GroupPostListResponse(group.getName(), groupPostListDto);
         } catch (BaseException e) {
             throw e;
         } catch (Exception e) {
--- src/main/java/com/lesso/neverland/post/repository/PostRepository.java
+++ src/main/java/com/lesso/neverland/post/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.lesso.neverland.post.repository;
 
 import com.lesso.neverland.common.enums.Contents;
+import com.lesso.neverland.group.domain.Team;
 import com.lesso.neverland.post.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findTop3ByPostTags_TagNameIn(List<Contents> interests);
+    List<Post> findByTeamAndStatusEqualsOrderByCreatedDateDesc(Team group, String status);
 }
