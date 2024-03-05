cfcbe0d4bb4ddb17b4cd53e00daebf625dbfcb48
joonghyun <joongh@ewhain.net>
2024-03-06 00:52:45 +0900

    #28 feat: comment request uri 추가

--- src/main/java/com/lesso/neverland/common/constants/RequestURI.java
+++ src/main/java/com/lesso/neverland/common/constants/RequestURI.java
@@ -4,4 +4,5 @@ public class RequestURI {
     public final static String user = "/users";
     public final static String post = "/posts";
     public final static String group = "/groups";
+    public final static String comment = "/comments";
 }
