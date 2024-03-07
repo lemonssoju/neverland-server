71d69fb5c9c9ce6b002ce84d449c64662b3fd48c
joonghyun <joongh@ewhain.net>
2024-03-07 21:59:56 +0900

    #14 chore: 피드 수정 화면 조회 관련 메서드명 변경

--- src/main/java/com/lesso/neverland/post/application/PostService.java
+++ src/main/java/com/lesso/neverland/post/application/PostService.java
@@ -85,13 +85,13 @@ public class PostService {
     }
 
     // [작성자] 피드 수정 화면 조회
-    public ModifyPostViewResponse getModifyPostView(Long postIdx) throws BaseException {
+    public PostEditViewResponse getPostEditView(Long postIdx) throws BaseException {
         try {
             User user = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
             Post post = postRepository.findById(postIdx).orElseThrow(() -> new BaseException(INVALID_POST_IDX));
             validateWriter(user, post);
 
-            return new ModifyPostViewResponse(post.getTitle(), post.getSubtitle(), post.getContentsType().getName(),
+            return new PostEditViewResponse(post.getTitle(), post.getSubtitle(), post.getContentsType().getName(),
                     post.getBackgroundMusic(), post.getBackgroundMusicUrl(), post.getPostImage(), post.getContent());
         } catch (BaseException e) {
             throw e;
--- src/main/java/com/lesso/neverland/post/presentation/PostController.java
+++ src/main/java/com/lesso/neverland/post/presentation/PostController.java
@@ -3,7 +3,7 @@ package com.lesso.neverland.post.presentation;
 import com.lesso.neverland.common.BaseException;
 import com.lesso.neverland.common.BaseResponse;
 import com.lesso.neverland.post.application.PostService;
-import com.lesso.neverland.post.dto.ModifyPostViewResponse;
+import com.lesso.neverland.post.dto.PostEditViewResponse;
 import com.lesso.neverland.post.dto.MyPostListResponse;
 import com.lesso.neverland.post.dto.PostResponse;
 import lombok.RequiredArgsConstructor;
@@ -30,9 +30,9 @@ public class PostController {
 
     // [작성자] 피드 수정 화면 조회
     @PatchMapping("/{postIdx}/editView")
-    public BaseResponse<ModifyPostViewResponse> getModifyPostView(@PathVariable Long postIdx) {
+    public BaseResponse<PostEditViewResponse> getPostEditView(@PathVariable Long postIdx) {
         try {
-            return new BaseResponse<>(postService.getModifyPostView(postIdx));
+            return new BaseResponse<>(postService.getPostEditView(postIdx));
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
         }
