c9731b8703dfd77f743723db00ae7a2fa094c659
joonghyun <joongh@ewhain.net>
2024-03-26 01:00:11 +0900

    #70 feat: GlobalExceptionHandler 생성

---
+++ src/main/java/com/lesso/neverland/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.lesso.neverland.common.exception;
+
+import com.lesso.neverland.common.BaseException;
+import com.lesso.neverland.common.ErrorResponse;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static com.lesso.neverland.common.BaseResponseStatus.DUPLICATE_RESOURCE;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = { ConstraintViolationException.class, DataIntegrityViolationException.class})
+    protected ResponseEntity<ErrorResponse> handleDataException() {
+        return ErrorResponse.toResponseEntity(DUPLICATE_RESOURCE);
+    }
+
+    @ExceptionHandler(value = { BaseException.class })
+    protected ResponseEntity<ErrorResponse> handleCustomException(BaseException e) {
+        return ErrorResponse.toResponseEntity(e.getStatus());
+    }
+}
