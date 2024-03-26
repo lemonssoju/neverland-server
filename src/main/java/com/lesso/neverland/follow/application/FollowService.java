236153a9a6dbe870c5431d10777cc1dbdd8416ce
joonghyun <joongh@ewhain.net>
2024-03-26 22:07:51 +0900

    #70 fix: BaseException이 RuntimeException 처리되도록 Follow 관련 로직 수정

--- src/main/java/com/lesso/neverland/follow/application/FollowService.java
+++ src/main/java/com/lesso/neverland/follow/application/FollowService.java
@@ -34,39 +34,27 @@ public class FollowService {
     UserTeamRepository userTeamRepository;
 
     // 팔로잉 목록 조회
-    public FollowingListResponse getFollowingList(String searchWord) throws BaseException {
-        try {
-            User user = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+    public FollowingListResponse getFollowingList(String searchWord) {
+        User user = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
 
-            List<FollowDto> followingList = Optional.ofNullable(searchWord)
-                    .map(search -> followRepository.findByFollowingUserAndFollowedNickname(user, search))
-                    .orElseGet(() -> followRepository.findByFollowingUserAndStatusEquals(user, ACTIVE))
-                    .stream()
-                    .map(this::convertToFollowDto).toList();
-            return new FollowingListResponse(followingList);
-        } catch (BaseException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new BaseException(DATABASE_ERROR);
-        }
+        List<FollowDto> followingList = Optional.ofNullable(searchWord)
+                .map(search -> followRepository.findByFollowingUserAndFollowedNickname(user, search))
+                .orElseGet(() -> followRepository.findByFollowingUserAndStatusEquals(user, ACTIVE))
+                .stream()
+                .map(this::convertToFollowDto).toList();
+        return new FollowingListResponse(followingList);
     }
 
     // 팔로워 목록 조회
-    public FollowerListResponse getFollowerList(String searchWord) throws BaseException {
-        try {
-            User user = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+    public FollowerListResponse getFollowerList(String searchWord) {
+        User user = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
 
-            List<FollowDto> followerList = Optional.ofNullable(searchWord)
-                    .map(search -> followRepository.findByFollowedUserAndFollowingNickname(user, search))
-                    .orElseGet(() -> followRepository.findByFollowedUserAndStatusEquals(user, ACTIVE))
-                    .stream()
-                    .map(this::convertToFollowDto).toList();
-            return new FollowerListResponse(followerList);
-        } catch (BaseException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new BaseException(DATABASE_ERROR);
-        }
+        List<FollowDto> followerList = Optional.ofNullable(searchWord)
+                .map(search -> followRepository.findByFollowedUserAndFollowingNickname(user, search))
+                .orElseGet(() -> followRepository.findByFollowedUserAndStatusEquals(user, ACTIVE))
+                .stream()
+                .map(this::convertToFollowDto).toList();
+        return new FollowerListResponse(followerList);
     }
 
     // FollowDto로 가공
@@ -78,29 +66,17 @@ public class FollowService {
     }
 
     // [그룹 생성] 맞팔로우 목록 조회
-    public MemberInviteListResponse getMemberInviteList(String searchWord) throws BaseException {
-        try {
-            User writer = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
-            return new MemberInviteListResponse(getMemberInviteDtoList(writer, follow -> false, searchWord));
-        } catch (BaseException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new BaseException(DATABASE_ERROR);
-        }
+    public MemberInviteListResponse getMemberInviteList(String searchWord) {
+        User writer = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+        return new MemberInviteListResponse(getMemberInviteDtoList(writer, follow -> false, searchWord));
     }
 
     // [그룹 수정] 맞팔로우 목록 조회
-    public MemberInviteListResponse getMemberInviteListEditView(Long groupIdx, String searchWord) throws BaseException {
-        try {
-            User writer = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
-            Team group = groupRepository.findById(groupIdx).orElseThrow(() -> new BaseException(INVALID_GROUP_IDX));
+    public MemberInviteListResponse getMemberInviteListEditView(Long groupIdx, String searchWord) {
+        User writer = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+        Team group = groupRepository.findById(groupIdx).orElseThrow(() -> new BaseException(INVALID_GROUP_IDX));
 
-            return new MemberInviteListResponse(getMemberInviteDtoList(writer, follow -> userTeamRepository.existsByUserAndTeam(follow.getFollowedUser(), group), searchWord));
-        } catch (BaseException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new BaseException(DATABASE_ERROR);
-        }
+        return new MemberInviteListResponse(getMemberInviteDtoList(writer, follow -> userTeamRepository.existsByUserAndTeam(follow.getFollowedUser(), group), searchWord));
     }
 
     private List<MemberInviteDto> getMemberInviteDtoList(User writer, Function<Follow, Boolean> isMemberFunction, String searchWord) {
@@ -117,29 +93,23 @@ public class FollowService {
     }
 
     // 팔로우/취소
-    public void follow(FollowRequest followRequest) throws BaseException {
-        try {
-            User followingUser = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
-            User followedUser = userRepository.findById(followRequest.userIdx()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+    public void follow(FollowRequest followRequest) {
+        User followingUser = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
+        User followedUser = userRepository.findById(followRequest.userIdx()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
 
-            Optional<Follow> optionalFollow = followRepository.findByFollowingUserAndFollowedUserAndStatusEquals(followingUser, followedUser, ACTIVE);
+        Optional<Follow> optionalFollow = followRepository.findByFollowingUserAndFollowedUserAndStatusEquals(followingUser, followedUser, ACTIVE);
 
-            Follow follow;
-            if (optionalFollow.isPresent()) { // 팔로우 기록이 이미 존재할 경우
-                follow = optionalFollow.get();
-                if (follow.getStatus().equals(ACTIVE)) { // 팔로우 상태면 언팔로우
-                    follow.unfollow();
-                } else { // 언팔로우 상태면 팔로우
-                    follow.follow();
-                }
-            } else { // 첫 팔로우
-                follow = new Follow(followingUser, followedUser);
+        Follow follow;
+        if (optionalFollow.isPresent()) { // 팔로우 기록이 이미 존재할 경우
+            follow = optionalFollow.get();
+            if (follow.getStatus().equals(ACTIVE)) { // 팔로우 상태면 언팔로우
+                follow.unfollow();
+            } else { // 언팔로우 상태면 팔로우
+                follow.follow();
             }
-            followRepository.save(follow);
-        } catch (BaseException e) {
-            throw e;
-        } catch (Exception e) {
-            throw new BaseException(DATABASE_ERROR);
+        } else { // 첫 팔로우
+            follow = new Follow(followingUser, followedUser);
         }
+        followRepository.save(follow);
     }
 }
