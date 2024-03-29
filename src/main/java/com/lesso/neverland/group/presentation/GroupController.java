2bfa19f8bffac2a63e2f751e5b02807fff2f7303
joonghyun <joongh@ewhain.net>
2024-03-29 14:04:08 +0900

    #20 fix: Group 관련 메서드 반환 형식 수정

--- src/main/java/com/lesso/neverland/group/presentation/GroupController.java
+++ src/main/java/com/lesso/neverland/group/presentation/GroupController.java
@@ -9,7 +9,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-import static com.lesso.neverland.common.BaseResponseStatus.SUCCESS;
 import static com.lesso.neverland.common.constants.RequestURI.group;
 
 @RestController
@@ -22,33 +21,32 @@ public class GroupController {
     // 그룹 목록 조회
     @GetMapping("")
     public BaseResponse<GroupListResponse> getGroupList() {
-        return new BaseResponse<>(groupService.getGroupList());
+        return groupService.getGroupList();
     }
 
     // 그룹 피드 목록 조회
     @GetMapping("/{groupIdx}/posts")
     public BaseResponse<GroupPostListResponse> getGroupPostList(@PathVariable Long groupIdx) {
-        return new BaseResponse<>(groupService.getGroupPostList(groupIdx));
+        return groupService.getGroupPostList(groupIdx);
     }
 
     // 그룹 피드 상세 조회
     @GetMapping("/{groupIdx}/posts/{postIdx}")
     public BaseResponse<GroupPostResponse> getGroupPostList(@PathVariable Long groupIdx, @PathVariable Long postIdx) {
-        return new BaseResponse<>(groupService.getGroupPost(groupIdx, postIdx));
+        return groupService.getGroupPost(groupIdx, postIdx);
     }
 
     // [관리자] 그룹 수정 화면 조회
     @GetMapping("/{groupIdx}/editView")
     public BaseResponse<GroupEditViewResponse> getGroupEditView(@PathVariable Long groupIdx) {
-        return new BaseResponse<>(groupService.getGroupEditView(groupIdx));
+        return groupService.getGroupEditView(groupIdx);
     }
 
     // [관리자] 그룹 수정
     @PatchMapping("/{groupIdx}")
     public BaseResponse<String> editGroup(@PathVariable Long groupIdx, @RequestPart MultipartFile image, @RequestPart EditGroupRequest editGroupRequest) {
         try {
-            groupService.editGroup(groupIdx, image, editGroupRequest);
-            return new BaseResponse<>(SUCCESS);
+            return groupService.editGroup(groupIdx, image, editGroupRequest);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -57,23 +55,20 @@ public class GroupController {
     // [관리자] 그룹 삭제
     @PatchMapping("/{groupIdx}/delete")
     public BaseResponse<String> deleteGroup(@PathVariable Long groupIdx) {
-        groupService.deleteGroup(groupIdx);
-        return new BaseResponse<>(SUCCESS);
+        return groupService.deleteGroup(groupIdx);
     }
 
     // 그룹 나가기
     @PatchMapping("/{groupIdx}/withdraw")
     public BaseResponse<String> withdrawGroup(@PathVariable Long groupIdx) {
-        groupService.withdrawGroup(groupIdx);
-        return new BaseResponse<>(SUCCESS);
+        return groupService.withdrawGroup(groupIdx);
     }
 
     // 그룹 생성
     @PostMapping("")
     public BaseResponse<String> createGroup(@RequestPart MultipartFile image, @RequestPart CreateGroupRequest createGroupRequest) {
         try {
-            groupService.createGroup(image, createGroupRequest);
-            return new BaseResponse<>(SUCCESS);
+            return groupService.createGroup(image, createGroupRequest);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -83,8 +78,7 @@ public class GroupController {
     @PostMapping("/{groupIdx}")
     public BaseResponse<String> createGroupPost(@PathVariable("groupIdx") Long groupIdx, @RequestPart MultipartFile image, @RequestPart GroupPostRequest groupPostRequest) {
         try {
-            groupService.createGroupPost(groupIdx, image, groupPostRequest);
-            return new BaseResponse<>(SUCCESS);
+            return groupService.createGroupPost(groupIdx, image, groupPostRequest);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
