fbec9f30c8c1eb3543a5cc8d7cff771f0ff41c8a
joonghyun <joongh@ewhain.net>
2024-04-28 13:36:58 +0900

    #79 fix: team entity에서 subName 삭제 및 startDate 추가

--- src/main/java/com/lesso/neverland/group/dto/GroupListDto.java
+++ src/main/java/com/lesso/neverland/group/dto/GroupListDto.java
@@ -2,5 +2,4 @@ package com.lesso.neverland.group.dto;
 
 public record GroupListDto(Long groupIdx,
                            String groupImage,
-                           String name,
-                           String subName) {}
+                           String name) {}
