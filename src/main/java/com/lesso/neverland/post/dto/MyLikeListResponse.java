269f9233582a879a996350818510f23e4f7aa1f1
joonghyun <joongh@ewhain.net>
2024-04-30 15:24:04 +0900

    #87 fix: post 관련 불필요한 파일 삭제

--- src/main/java/com/lesso/neverland/post/dto/MyLikeListResponse.java
+++
@@ -1,6 +0,0 @@
-package com.lesso.neverland.post.dto;
-
-import java.util.List;
-
-public record MyLikeListResponse(List<MyPostDto> myLikeList) {
-}
--- src/main/java/com/lesso/neverland/post/dto/RecommendedPostDto.java
+++
@@ -1,7 +0,0 @@
-package com.lesso.neverland.post.dto;
-
-import java.util.List;
-
-public record RecommendedPostDto(String postImage,
-                                 String title,
-                                 List<String> tags) {}
