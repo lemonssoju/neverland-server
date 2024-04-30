b9e4966dcbe097d9ce169272633abc6a771d5d99
joonghyun <joongh@ewhain.net>
2024-04-30 16:30:32 +0900

    #99 fix: GroupController, GroupService에서 puzzle 관련 부분 수정

--- src/main/java/com/lesso/neverland/group/application/GroupService.java
+++ src/main/java/com/lesso/neverland/group/application/GroupService.java
@@ -1,22 +1,14 @@
 package com.lesso.neverland.group.application;
 
-import com.lesso.neverland.comment.domain.Comment;
-import com.lesso.neverland.comment.dto.CommentDto;
 import com.lesso.neverland.comment.repository.CommentRepository;
 import com.lesso.neverland.common.base.BaseException;
 import com.lesso.neverland.common.base.BaseResponse;
-import com.lesso.neverland.common.enums.Contents;
-import com.lesso.neverland.common.enums.Source;
 import com.lesso.neverland.common.image.ImageService;
 import com.lesso.neverland.group.domain.Team;
 import com.lesso.neverland.group.dto.*;
 import com.lesso.neverland.group.repository.GroupRepository;
-import com.lesso.neverland.post.domain.Post;
-import com.lesso.neverland.post.domain.PostLike;
-import com.lesso.neverland.post.domain.PostTag;
-import com.lesso.neverland.post.repository.PostLikeRepository;
-import com.lesso.neverland.post.repository.PostRepository;
-import com.lesso.neverland.post.repository.PostTagRepository;
+import com.lesso.neverland.puzzle.domain.Puzzle;
+import com.lesso.neverland.puzzle.repository.PuzzleRepository;
 import com.lesso.neverland.user.application.AuthService;
 import com.lesso.neverland.user.application.UserService;
 import com.lesso.neverland.user.domain.User;
@@ -29,9 +21,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static com.lesso.neverland.common.base.BaseResponseStatus.*;
@@ -44,13 +36,11 @@ public class GroupService {
     private final UserService userService;
     private final UserRepository userRepository;
     private final GroupRepository groupRepository;
-    private final PostRepository postRepository;
+    private final PuzzleRepository puzzleRepository;
     private final AuthService authService;
-    private final PostLikeRepository postLikeRepository;
     private final ImageService imageService;
     private final UserTeamRepository userTeamRepository;
     private final CommentRepository commentRepository;
-    private final PostTagRepository postTagRepository;
 
     // 그룹 목록 조회
     public BaseResponse<GroupListResponse> getGroupList() {
@@ -64,49 +54,29 @@ public class GroupService {
         return new BaseResponse<>(new GroupListResponse(groupListDto));
     }
 
-    // 그룹 피드 목록 조회
-    public BaseResponse<GroupPostListResponse> getGroupPostList(Long groupIdx) {
+    // 그룹 퍼즐 목록 조회
+    public BaseResponse<GroupPuzzleListResponse> getGroupPuzzleList(Long groupIdx) {
         User user = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
         Team group = groupRepository.findById(groupIdx).orElseThrow(() -> new BaseException(INVALID_GROUP_IDX));
-        List<Post> groupPostList = postRepository.findByTeamAndStatusEqualsOrderByCreatedDateDesc(group, ACTIVE);
+        List<Puzzle> groupPuzzleList = puzzleRepository.findByTeamAndStatusEqualsOrderByCreatedDateDesc(group, ACTIVE);
 
-        List<GroupPostDto> groupPostListDto = groupPostList.stream()
-                .map(groupPost -> new GroupPostDto(
-                        groupPost.getTitle(),
-                        groupPost.getSubtitle(),
-                        groupPost.getPostImage(),
-                        groupPost.getUser().getProfile().getNickname())).collect(Collectors.toList());
-        return new BaseResponse<>(new GroupPostListResponse(group.getName(), groupPostListDto));
+        List<GroupPuzzleDto> groupPuzzleListDto = groupPuzzleList.stream()
+                .map(groupPuzzle -> new GroupPuzzleDto(
+                        groupPuzzle.getTitle(),
+                        groupPuzzle.getPuzzleImage(),
+                        groupPuzzle.getUser().getProfile().getNickname())).collect(Collectors.toList());
+        return new BaseResponse<>(new GroupPuzzleListResponse(group.getName(), groupPuzzleListDto));
     }
 
-    // 그룹 피드 상세 조회
-    public BaseResponse<GroupPostResponse> getGroupPost(Long groupIdx, Long postIdx) {
+    // 그룹 퍼즐 상세 조회
+    public BaseResponse<GroupPuzzleResponse> getGroupPuzzle(Long groupIdx, Long puzzleIdx) {
         Team group = groupRepository.findById(groupIdx).orElseThrow(() -> new BaseException(INVALID_GROUP_IDX));
-        Post post = postRepository.findById(postIdx).orElseThrow(() -> new BaseException(INVALID_POST_IDX));
-        if (!post.getTeam().equals(group)) throw new BaseException(NO_GROUP_POST);
+        Puzzle puzzle = puzzleRepository.findById(puzzleIdx).orElseThrow(() -> new BaseException(INVALID_PUZZLE_IDX));
+        if (!puzzle.getTeam().equals(group)) throw new BaseException(NO_GROUP_PUZZLE);
 
-        // 좋아요 여부 조회
-        Optional<Long> optionalUserIdx = Optional.ofNullable(authService.getUserIdx());
-        boolean isLike = false;
-        if (optionalUserIdx.isPresent()){
-            User user = userRepository.findById(optionalUserIdx.get()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
-            isLike = postLikeRepository.existsByPostAndUserAndStatusEquals(post, user, ACTIVE);
-        }
-
-        // 댓글 조회
-        List<Comment> postComments = post.getComments();
-        List<CommentDto> comments = postComments.stream()
-                .map(comment -> new CommentDto(
-                        comment.getCommentIdx(),
-                        comment.getUser().getProfile().getNickname(),
-                        comment.getUser().getProfile().getProfileImage(),
-                        comment.getCreatedDate(),
-                        comment.getContent()))
-                .collect(Collectors.toList());
-
-        return new BaseResponse<>(new GroupPostResponse(post.getTitle(), post.getSubtitle(), post.getContent(), post.getCreatedDate(),
-                post.getUser().getProfile().getNickname(), post.getBackgroundMusic(), post.getBackgroundMusicUrl(),
-                post.getPostImage(), isLike, comments));
+        return new BaseResponse<>(new GroupPuzzleResponse(puzzle.getTitle(), puzzle.getContent(), puzzle.getCreatedDate(),
+                puzzle.getUser().getProfile().getNickname(), puzzle.getBackgroundMusic(), puzzle.getBackgroundMusicUrl(),
+                puzzle.getPuzzleImage()));
     }
 
     // [관리자] 그룹 수정 화면 조회
@@ -163,35 +133,14 @@ public class GroupService {
         User user = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
         validateAdmin(user, group);
 
-        List<Post> groupPostList = postRepository.findByTeamAndStatusEquals(group, ACTIVE);
-        List<Comment> commentsSaveList = new ArrayList<>();
-        List<PostLike> postLikesSaveList = new ArrayList<>();
-        List<PostTag> postTagsSaveList = new ArrayList<>();
+        List<Puzzle> groupPuzzleList = puzzleRepository.findByTeamAndStatusEquals(group, ACTIVE);
         List<UserTeam> userTeamsSaveList = new ArrayList<>();
 
-        for (Post post : groupPostList) {
-            post.delete();
-            for(Comment comment : post.getComments()) {
-                comment.delete();
-                commentsSaveList.add(comment);
-            }
-            for(PostLike postLike : post.getPostLikes()) {
-                postLike.delete();
-                postLikesSaveList.add(postLike);
-            }
-            for(PostTag postTag : post.getPostTags()) {
-                postTag.delete();
-                postTagsSaveList.add(postTag);
-            }
-        }
         for (UserTeam userTeam : group.getUserTeams()) {
             userTeam.delete();
             userTeamsSaveList.add(userTeam);
         }
-        postRepository.saveAll(groupPostList);
-        commentRepository.saveAll(commentsSaveList);
-        postLikeRepository.saveAll(postLikesSaveList);
-        postTagRepository.saveAll(postTagsSaveList);
+        puzzleRepository.saveAll(groupPuzzleList);
         userTeamRepository.saveAll(userTeamsSaveList);
 
         group.delete();
@@ -241,18 +190,20 @@ public class GroupService {
     }
 
     // 그룹 피드 등록
-    public BaseResponse<String> createGroupPost(Long groupIdx, MultipartFile image, GroupPostRequest groupPostRequest) throws IOException {
+    public BaseResponse<String> createGroupPuzzle(Long groupIdx, MultipartFile image, GroupPuzzleRequest groupPuzzleRequest) throws IOException {
         Team group = groupRepository.findById(groupIdx).orElseThrow(() -> new BaseException(INVALID_GROUP_IDX));
         User writer = userRepository.findById(userService.getUserIdxWithValidation()).orElseThrow(() -> new BaseException(INVALID_USER_IDX));
 
         // upload image
         String imagePath = imageService.uploadImage("group", image);
 
-        Contents contentsType = Contents.getEnumByName(groupPostRequest.contentsType());
-        // TODO: gpt 활용한 태그 생성
-        Post post = new Post(writer, Source.USER, group, groupPostRequest.title(), groupPostRequest.subtitle(),
-                contentsType, groupPostRequest.backgroundMusic(), groupPostRequest.backgroundMusicUrl(), imagePath, groupPostRequest.content());
-        postRepository.save(post);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MMM-dd");
+        formatter = formatter.withLocale(Locale.KOREA);
+        LocalDate puzzleDate = LocalDate.parse(groupPuzzleRequest.puzzleDate(), formatter);
+
+        Puzzle puzzle = new Puzzle(writer, group, groupPuzzleRequest.title(), groupPuzzleRequest.content(),
+                imagePath, puzzleDate, groupPuzzleRequest.location(), groupPuzzleRequest.backgroundMusic(), groupPuzzleRequest.backgroundMusicUrl());
+        puzzleRepository.save(puzzle);
         return new BaseResponse<>(SUCCESS);
     }
 
