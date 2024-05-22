e5743d944c90522ba0bac020eb76a9fca74e9073
joonghyun <joongh@ewhain.net>
2024-05-23 00:22:59 +0900

    #122 fix: 퍼즐 목록 dto에 생성일자, 위치 추가

--- src/main/java/com/lesso/neverland/group/dto/GroupPuzzleDto.java
+++ src/main/java/com/lesso/neverland/group/dto/GroupPuzzleDto.java
@@ -2,4 +2,6 @@ package com.lesso.neverland.group.dto;
 
 public record GroupPuzzleDto(String title,
                              String puzzleImage,
-                             String writer) {}
+                             String writer,
+                             String createdDate,
+                             String location) {}
